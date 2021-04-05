1ef81a936ce3d86820f57d4aecf8514869108f87
RamssesArenas <l15200119@pachuca.tecnm.mx>
2021-04-05 15:59:34 -0500

    Leyendo el estado

---
+++ src/commponents/Formulario.js
@@ -0,0 +1,81 @@
+import React,{Fragment, useState} from 'react';
+
+const Formulario = () => {
+
+  //Crear state de Citas
+  const [cita, actualizarCita] = useState({ 
+    mascota:'',
+    propietario:'',
+    fecha:'',
+    hora:'',
+    sintomas:''
+  });
+  //Funcion que se ejecuta cada quel usuario escribe en un input
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]:e.target.value
+    })
+  }
+  //Extraer Valores
+  const {mascota,propietario,fecha,hora,sintomas} = cita;
+
+  return (
+    <Fragment>
+      <h2>Crear Citas</h2>
+      <form>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre del dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+        
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+        <label>Síntomas</label>
+        <textarea
+          type="submit"
+          className="u-full-width"
+          name="sintomas"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button
+          type="submit"
+          className="u-full-width button-primary" 
+        >Agregar Cita</button>
+      </form>
+    </Fragment>
+  );
+}
+
+export default Formulario; 
